4c891e9acfe505b404241e92c4eed763552c96e3
nazar-dmytrenko <nazarkadm@mail.com>
2026-02-17 17:13:25 +0200

    soulution v2

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -142,7 +142,7 @@ async def delete_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
     if not movie:
         raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
 
-    await db.delete(movie)
+    db.delete(movie)
     await db.commit()
     return None
 
